4fc6d55ab608ff2a291433501ba18c18de687e93
purvin_itpath <purvin.itpath@gmail.com>
2025-10-14 11:56:41 +0530

    add images

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+export const Images = {
+  register: require('./images/regfinal.png'),
+  design: require('./images/design.png'),
+};
+export const Icons = {
+  bell: require('./icons/bell.png'),
+  location: require('./icons/location.png'),
+  downarrow: require('./icons/arrow-down.png'),
+  loupe: require('./icons/loupe.png'),
+};
